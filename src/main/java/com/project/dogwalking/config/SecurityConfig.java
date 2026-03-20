ffa12e1918f7f83b61cce2b8627f8c44a7a88411
woklze <karikaturis02@mail.ru>
2026-03-20 12:49:35 +0500

    Update SecurityConfig.java

--- src/main/java/com/project/dogwalking/config/SecurityConfig.java
+++ src/main/java/com/project/dogwalking/config/SecurityConfig.java
@@ -20,8 +20,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // новый способ отключения CSRF
-                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()); // разрешаем все запросы
+                .csrf(csrf -> csrf.disable()) 
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
         return http.build();
     }
 }
