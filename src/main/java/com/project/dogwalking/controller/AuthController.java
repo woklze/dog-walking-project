82d61c40bc9d15ceb47fd0c72aa831655de331e6
nastiicZ <zolotareva.nastena.09@mail.ru>
2026-05-14 21:37:37 +0500

    AuthController.java

--- src/main/java/com/project/dogwalking/controller/AuthController.java
+++ src/main/java/com/project/dogwalking/controller/AuthController.java
@@ -4,7 +4,10 @@ import com.project.dogwalking.dto.UserLoginDto;
 import com.project.dogwalking.dto.UserRegistrationDto;
 import com.project.dogwalking.dto.UserResponseDto;
 import com.project.dogwalking.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,17 +19,32 @@ import jakarta.validation.Valid;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final UserService userService;
+    @Autowired
+    private UserService userService;
+
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponseDto> register(@Valid @RequestBody UserRegistrationDto registrationDto) {
-        UserResponseDto createdUser = userService.register(registrationDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    public ResponseEntity<?> register(@Valid @RequestBody UserRegistrationDto regDto) {
+        userService.register(regDto);
+        return ResponseEntity.ok("Пользователь зарегистрирован");
     }
 
     @PostMapping("/login")
-    public ResponseEntity<UserResponseDto> login(@Valid @RequestBody UserLoginDto loginDto) {
-        // заглушка
-        throw new UnsupportedOperationException("Вход в аккунт еще не реализован");
+    public ResponseEntity<?> login(@Valid @RequestBody UserLoginDto loginDto, HttpSession session) {
+        if (userService.authenticate(loginDto.getEmail(), loginDto.getPassword())) {
+            session.setAttribute("userEmail", loginDto.getEmail());
+            session.setAttribute("isAuthenticated", true);
+            session.setMaxInactiveInterval(3600); // Таймаут 1 час (в секундах)
+            return ResponseEntity.ok("Успешный вход");
+        } else {
+            return ResponseEntity.status(401).body("Неверные учетные данные");
+        }
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpSession session) {
+
+        session.invalidate();
+        return ResponseEntity.ok("Успешный выход из системы");
     }
 }
